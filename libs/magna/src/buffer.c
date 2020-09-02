7fc4293955a90b10914a537c8972788d7a8f6635
Alexey Mironov <amironov73@gmail.com>
2020-09-02 16:20:45 +0800

    Working on buffer.c

--- libs/magna/src/buffer.c
+++ libs/magna/src/buffer.c
@@ -38,9 +38,10 @@ MAGNA_API am_size_t MAGNA_CALL buffer_calculate_size
 }
 
 /**
- * Клонирование буфера.
+ * Клонирование буфера. Происходит полное копирование памяти исходного буфера.
  *
- * @param buffer
+ * @param target Неинициализированный буфер назначения.
+ * @param source Буфер, подлежащий клонированию.
  * @return
  */
 MAGNA_API Buffer* MAGNA_CALL buffer_clone
@@ -65,10 +66,11 @@ MAGNA_API Buffer* MAGNA_CALL buffer_clone
 }
 
 /**
- * Копирует один буфер в другой.
+ * Копирует один буфер в другой. Происходит полное копирование памяти
+ * исходного буфера.
  *
- * @param target
- * @param source
+ * @param target Инициализированный буфер назначения.
+ * @param source Исходный буфер.
  */
 MAGNA_API am_bool MAGNA_CALL buffer_copy
     (
@@ -82,6 +84,8 @@ MAGNA_API am_bool MAGNA_CALL buffer_copy
     if (!buffer_grow (target, source->position)) {
         return AM_FALSE;
     }
+
+    target->position = 0;
 
     if (source->position != 0) {
         memcpy(target->ptr, source->ptr, source->position);
@@ -144,7 +148,7 @@ MAGNA_API am_bool MAGNA_CALL buffer_create
         }
 
         if (data != NULL) {
-            memcpy(buffer->ptr, data, length);
+            memcpy (buffer->ptr, data, length);
             buffer->position = length;
         }
     }
@@ -210,7 +214,6 @@ MAGNA_API am_bool MAGNA_CALL buffer_grow
     am_byte *newPtr;
 
     assert (buffer != NULL);
-    assert (newSize > 0);
 
     if (newSize > buffer->capacity) {
         newSize = buffer_calculate_size (newSize);
@@ -235,7 +238,7 @@ MAGNA_API am_bool MAGNA_CALL buffer_grow
  * @param buffer
  * @param c
  */
-MAGNA_API void MAGNA_CALL buffer_putc
+MAGNA_API am_bool MAGNA_CALL buffer_putc
     (
         Buffer *buffer,
         am_byte c
@@ -243,8 +246,12 @@ MAGNA_API void MAGNA_CALL buffer_putc
 {
     assert (buffer != NULL);
 
-    buffer_grow (buffer, buffer->position + 1);
+    if (!buffer_grow (buffer, buffer->position + 1)) {
+        return AM_FALSE;
+    }
     buffer->ptr [buffer->position++] = c;
+
+    return AM_TRUE;
 }
 
 /**
@@ -253,7 +260,7 @@ MAGNA_API void MAGNA_CALL buffer_putc
  * @param buffer
  * @param str
  */
-MAGNA_API void MAGNA_CALL buffer_puts
+MAGNA_API am_bool MAGNA_CALL buffer_puts
     (
         Buffer *buffer,
         const char *str
@@ -263,9 +270,14 @@ MAGNA_API void MAGNA_CALL buffer_puts
     assert (str != NULL);
 
     size_t delta = strlen (str);
-    buffer_grow (buffer, buffer->position + delta);
+    if (!buffer_grow (buffer, buffer->position + delta)) {
+        return AM_FALSE;
+    }
+
     memcpy (buffer->ptr + buffer->position, str, delta);
     buffer->position += delta;
+
+    return AM_TRUE;
 }
 
 /**
@@ -277,9 +289,9 @@ MAGNA_API void MAGNA_CALL buffer_puts
  */
 MAGNA_API void MAGNA_CALL buffer_write
     (
-            Buffer *buffer,
-            const am_byte *data,
-            am_size_t length
+        Buffer *buffer,
+        const am_byte *data,
+        am_size_t length
     )
 {
     assert (buffer != NULL);
@@ -300,9 +312,9 @@ MAGNA_API void MAGNA_CALL buffer_write
  */
 MAGNA_API Buffer* MAGNA_CALL buffer_assign
     (
-            Buffer *buffer,
-            const am_byte *data,
-            am_size_t length
+        Buffer *buffer,
+        const am_byte *data,
+        am_size_t length
     )
 {
     assert (buffer != NULL);
@@ -311,7 +323,10 @@ MAGNA_API Buffer* MAGNA_CALL buffer_assign
         buffer->position = 0;
     }
     else {
-        buffer_grow (buffer, length);
+        if (!buffer_grow (buffer, length)) {
+            return NULL;
+        }
+
         memcpy (buffer->ptr, data, length);
         buffer->position = length;
     }
@@ -424,6 +439,204 @@ MAGNA_API Buffer* MAGNA_CALL buffer_from_span
     buffer->capacity = span.len;
 
     return buffer;
+}
+
+static am_byte* find_text
+    (
+        const am_byte *begin,
+        const am_byte *end,
+        const am_byte *what
+    )
+{
+    const am_byte *ptr1, *ptr2, *ptr3;
+    am_bool found;
+
+    for (ptr1 = begin; ptr1 < end; ++ptr1) {
+        found = AM_TRUE;
+        for (ptr2 = ptr1, ptr3 = what; *ptr3; ++ptr2, ++ptr3) {
+            if (*ptr2 != *ptr3) {
+                found = AM_FALSE;
+                break;
+            }
+        }
+        if (found) {
+            return (am_byte*) ptr1;
+        }
+    }
+
+    return NULL;
+}
+
+/**
+ * Замена текста в буфере.
+ *
+ * @param buffer Буфер.
+ * @param from Заменяемый текст.
+ * @param to Заменяющий текст.
+ * @return Признак успешности завершения операции.
+ */
+MAGNA_API am_bool MAGNA_CALL buffer_replace_text
+    (
+        Buffer *buffer,
+        const am_byte *from,
+        const am_byte *to
+    )
+{
+    am_byte *ptr1, *ptr2, *end;
+    am_size_t lenFrom, lenTo, count, offset;
+
+    assert (buffer != NULL);
+    assert (from != NULL);
+    assert (to != NULL);
+
+    if (buffer->position == 0) {
+        /* В пустом буфере нам нечего делать */
+        return AM_TRUE;
+    }
+
+    lenFrom = strlen (from);
+    lenTo = strlen (to);
+    end = buffer->ptr + buffer->position;
+    for (ptr1 = buffer->ptr; ptr1 < end; ) {
+        ptr2 = find_text (ptr1, end, from);
+        if (ptr2) {
+            /* нашли заменяемую строку */
+            if (lenFrom < lenTo) {
+                /* заменяющая строка длинее */
+                offset = ptr2 - buffer->ptr;
+                if (!buffer_grow (buffer, buffer->position + lenTo - lenFrom)) {
+                    return AM_FALSE;
+                }
+                /* Из-за переаллокации буфера указатель мог измениться */
+                ptr2 = buffer->ptr + offset;
+                count = end - ptr2 - lenFrom;
+                if (count > 0) {
+                    memmove (ptr2 + lenTo, ptr2 + lenFrom, count);
+                }
+            }
+            else if (lenFrom > lenTo) {
+                count = end - ptr2 - lenTo;
+                if (count > 0) {
+                    memmove (ptr2 + lenTo, ptr2 + lenFrom, count);
+                }
+            }
+
+            /* копируем заменяющую строку */
+            memcpy (ptr2, to, lenTo);
+            buffer->position += (lenTo - lenFrom);
+            end = buffer->ptr + buffer->position;
+            ptr1 = ptr2 + lenTo;
+        }
+        else {
+            /* заменяемая строка не найдена */
+            break;
+        }
+    }
+
+    return AM_TRUE;
+}
+
+/**
+ * Побайтовое сравнение двух буферов.
+ *
+ * @param first
+ * @param second
+ * @return
+ */
+MAGNA_API int MAGNA_CALL buffer_compare
+    (
+        const Buffer *first,
+        const Buffer *second
+    )
+{
+    am_size_t i;
+    int result;
+
+    assert (first != NULL);
+    assert (second != NULL);
+
+    for (i = 0; ; ++i) {
+        if (i == first->position) {
+            if (i == second->position) {
+                return 0;
+            }
+
+            return -1;
+        }
+        else if (i == second->position) {
+                return 1;
+            }
+        else {
+            result = first->ptr[i] - second->ptr[i];
+            if (result != 0) {
+                return result;
+            }
+        }
+    }
+}
+
+/**
+ * Побайтовое сравнение буфера со строкой.
+ *
+ * @param buffer
+ * @param text
+ * @return
+ */
+MAGNA_API int MAGNA_CALL buffer_compare_text
+    (
+        const Buffer *buffer,
+        const am_byte *text
+    )
+{
+    const am_byte *ptr = text;
+    am_size_t i;
+    int result;
+
+    assert (buffer != NULL);
+    assert (text != NULL);
+
+    for (i = 0; ; ++i, ++ptr) {
+        if (i == buffer->position) {
+            if (!*ptr) {
+                return 0;
+            }
+
+            return -1;
+        }
+        else if (!ptr) {
+            return 1;
+        }
+        else {
+            result = buffer->ptr[i] - *ptr;
+            if (result != 0) {
+                return result;
+            }
+        }
+    }
+}
+
+/**
+ * Поиск первого вхождения строки в буфере.
+ *
+ * @param buffer
+ * @param text
+ * @return
+ */
+MAGNA_API am_byte* MAGNA_CALL buffer_find_text
+    (
+        const Buffer *buffer,
+        const am_byte *text
+    )
+{
+    assert (buffer != NULL);
+    assert (text != NULL);
+
+    return find_text
+        (
+            buffer->ptr,
+            buffer->ptr + buffer->position,
+            text
+        );
 }
 
 /*=========================================================*/
--- tests/offline/src/buffer.c
+++ tests/offline/src/buffer.c
@@ -55,3 +55,101 @@ TESTER(buffer_static_2)
     CHECK (buffer.capacity == 0);
 }
 
+TESTER(buffer_replace_1)
+{
+    am_byte data[] = "У попа была собака. Он ее любил";
+    Buffer buffer;
+
+    CHECK (buffer_create
+        (
+            &buffer,
+            data,
+            sizeof (data) - 1 /* Без завершающего нуля */
+        ));
+    CHECK (buffer_replace_text (&buffer, "собака", "СБЕРКНИЖКА"));
+    CHECK (buffer_compare_text (&buffer, "У попа была СБЕРКНИЖКА. Он ее любил") == 0);
+    buffer_free (&buffer);
+}
+
+TESTER(buffer_find_text_1)
+{
+    am_byte data[] = "У попа была собака. Он ее любил";
+    Buffer buffer;
+    const am_byte *ptr;
+
+    buffer_static (&buffer, data, sizeof (data) - 1);
+    buffer.position = buffer.capacity;
+    ptr = buffer_find_text (&buffer, "собака");
+    CHECK ((ptr - data) == 21);
+}
+
+TESTER(buffer_copy_1)
+{
+    am_byte data[] = "У попа была собака. Он ее любил";
+    Buffer target = BUFFER_INIT, source;
+
+    buffer_static (&source, data, sizeof (data) - 1);
+    source.position = source.capacity;
+    buffer_copy (&target, &source);
+    CHECK (target.position == source.position);
+    CHECK (memcmp (target.ptr, source.ptr, source.position) == 0);
+    buffer_free (&target);
+}
+
+TESTER(buffer_concat_1)
+{
+    am_byte data1[] = "Hello, ", data2[] = "world!";
+    Buffer buffer1, buffer2, buffer3 = BUFFER_INIT;
+
+    buffer_static (&buffer1, data1, sizeof (data1) - 1);
+    buffer_static (&buffer2, data2, sizeof (data2) - 1);
+    buffer_concat (&buffer3, &buffer1);
+    buffer_concat (&buffer3, &buffer2);
+    CHECK (buffer_compare_text (&buffer3, "Hello, world!"));
+}
+
+TESTER(buffer_putc_1)
+{
+    Buffer buffer = BUFFER_INIT;
+
+    buffer_putc (&buffer, 'H');
+    buffer_putc (&buffer, 'e');
+    buffer_putc (&buffer, 'l');
+    buffer_putc (&buffer, 'l');
+    buffer_putc (&buffer, 'o');
+    buffer_putc (&buffer, '!');
+    CHECK (buffer_compare_text (&buffer, "Hello!") == 0);
+}
+
+TESTER(buffer_puts_1)
+{
+    Buffer buffer = BUFFER_INIT;
+
+    buffer_puts (&buffer, "Hello,");
+    buffer_puts (&buffer, " ");
+    buffer_puts (&buffer, "world!");
+    CHECK (buffer_compare_text (&buffer, "Hello, world!") == 0);
+}
+
+TESTER(buffer_from_span_1)
+{
+    am_byte data[] = { 1, 2, 3, 4, 5, 6, 7 };
+    Span span = span_init (data, sizeof (data));
+    Buffer buffer;
+
+    buffer_from_span (&buffer, span);
+    CHECK (buffer.ptr == span.ptr);
+    CHECK (buffer.position == 0);
+    CHECK (buffer.capacity == span.len);
+}
+
+TESTER(buffer_assign_1)
+{
+    am_byte data[] = { 1, 2, 3, 4, 5, 6, 7 };
+    Buffer buffer = BUFFER_INIT;
+
+    buffer_assign (&buffer, data, sizeof (data));
+    CHECK (buffer.position == sizeof (data));
+    CHECK (buffer.ptr[0] == 1);
+    CHECK (buffer.ptr[6] == 7);
+}
